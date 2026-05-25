51fb7814b721f911bc19a58feb5590529c211c4a
Vova <vovakucin082@gmail.com>
2026-05-25 09:21:48 +0200

    add book view and register in admin.py

--- books/serializers.py
+++ books/serializers.py
@@ -3,7 +3,7 @@ from rest_framework import serializers
 from books.models import Book
 
 
-class BooksSerializer(serializers.ModelSerializer):
+class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
         fields = ("id", "title", "author", "cover", "inventory", "daily_fee")
--- books/views.py
+++ books/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from books.models import Book
+from books.serializers import BookSerializer
+
+
+class BookView(ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
