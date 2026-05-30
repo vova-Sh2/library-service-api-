4706add4e1ce559df9bf975be1beb40678462f41
Vova <vovakucin082@gmail.com>
2026-05-30 09:22:05 +0200

    Update README.md file

--- library_service_api/settings/base.py
+++ library_service_api/settings/base.py
@@ -145,7 +145,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(days=5),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     "ROTATE_REFRESH_TOKENS": False,
     "AUTH_HEADER_NAME": "HTTP_AUTHORIZE",
