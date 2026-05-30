74468f186446ad27c69115160fda174807d3ae4b
Vova <vovakucin082@gmail.com>
2026-05-30 09:43:52 +0200

    Update max_len session_id, session_url to 500

--- payments/models.py
+++ payments/models.py
@@ -5,12 +5,12 @@ from borrowings.models import Borrowing
 
 class Payment(models.Model):
     class StausChoices(models.TextChoices):
-        PENDING = "Pending"
-        PAID = "Paid"
+        PENDING = "PENDING", "Pending"
+        PAID = "PAID", "Paid"
 
     class TypeChoices(models.TextChoices):
-        PAYMENT = "Payment"
-        FINE = "Fine"
+        PAYMENT = "PAYMENT", "Payment"
+        FINE = "FINE", "Fine"
 
     status = models.CharField(
         max_length=10, choices=StausChoices.choices, default=StausChoices.PENDING
@@ -21,6 +21,6 @@ class Payment(models.Model):
     borrowing = models.ForeignKey(
         Borrowing, on_delete=models.CASCADE, related_name="payments"
     )
-    session_url = models.URLField(null=True, blank=True)
-    session_id = models.CharField(max_length=155, null=True, blank=True)
+    session_url = models.URLField(max_length=500, null=True, blank=True)
+    session_id = models.CharField(max_length=500, null=True, blank=True)
     money_to_pay = models.DecimalField(max_digits=8, decimal_places=2)
